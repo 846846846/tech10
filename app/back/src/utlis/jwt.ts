728bd0cd5f02a40ddb4867c6a2c652cb5ec0ed62
846846846 <your.email@example.com>
2024-04-09 05:03:45 +0900

    UT対応 中間コミット5

--- app/back/src/utlis/jwt.ts
+++ app/back/src/utlis/jwt.ts
@@ -5,14 +5,18 @@ export default class JWTWrap {
 
   constructor(authorization: string | undefined) {
     try {
+      console.log(11)
       if (authorization === undefined) throw new CustomError(400, 'authorizationヘッダが見つかりません')
+      console.log(12)
 
       // Beaerを取り除く.
       const idToken = authorization.split(' ')[1]
       if (idToken === undefined) throw new CustomError(400, 'idTokenが見つかりません')
 
       this.decodedToken = jwt.decode(idToken) as jwt.JwtPayload
+      console.log(13)
     } catch (err) {
+      console.log(14)
       throw err
     }
   }
